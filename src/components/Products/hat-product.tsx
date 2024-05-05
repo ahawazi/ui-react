032c4a891f7ba07010842d92a911b5980d12d3ab
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-05 18:38:58 +0330

    add Product (Hat) component

---
+++ src/components/Products/hat-product.tsx
@@ -0,0 +1,44 @@
+<div className="border-gray-400 border-2 rounded-xl h-fit m-11 p-2">
+  <div className="">
+    <div>
+      <img
+        src="https://via.placeholder.com/150"
+        alt="Hat"
+        className="overflow-hidden"
+      />
+    </div>
+    <div className="text-center font-bold p-1">کلاه حصیر دستبافت</div>
+    <div className="w-full h-1 bg-gray-300 justify-center"></div>
+    <div className="flex gap-3 justify-between mt-3">
+      <button className="border-red-500 border-2 rounded-xl w-12 text-center text-red-500">
+        <a href="#">خرید</a>
+      </button>
+      <div className="flex items-center">
+        <div className="text-gray-400">راور,کرمان</div>
+        <div className="size-4">
+          <svg
+            fill="#e71313"
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 395.71 395.71"
+          >
+            <g id="SVGRepo_bgCarrier" stroke-width="0" />
+            <g
+              id="SVGRepo_tracerCarrier"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <g>
+                {" "}
+                <path d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738 c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388 C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191 c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z" />{" "}
+              </g>{" "}
+            </g>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
