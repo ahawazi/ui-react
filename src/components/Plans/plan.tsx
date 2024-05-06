cf1e2993e5f121125b05a45fe492cc45b03c9c14
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-06 19:22:20 +0330

    add the plan components

---
+++ src/components/Plans/plan.tsx
@@ -0,0 +1,34 @@
+<div className="border-gray-400 border-2 rounded-xl h-fit m-11 size-fit">
+  <img
+    src="https://via.placeholder.com/300"
+    alt="Plan"
+    className="rounded-t-lg"
+  />
+  <div className="p-2">
+    <div>
+      <p className="font-semibold">
+        بیست و پنجمین مرحله ثبت نام طرح فرزندان
+        <br /> غدیر
+      </p>
+      <p className="text-gray-600 ">
+        در کنار شما قدم های بزرگی برای شادی دل
+        <br /> مردم عزیز و شعیان و محبان اهل بیت <br />
+        (علیهم سلام) برداشته ایم.
+      </p>
+    </div>
+
+    <div className="w-full h-1 bg-gray-300 justify-center"></div>
+
+    <div className="grid grid-cols-2">
+      <div></div>
+      <div className="flex gap-2 justify-end m-2">
+        <div>1402\8\17</div>
+        <img
+          className="size-6"
+          src="https://uxwing.com/wp-content/themes/uxwing/download/time-and-date/date-and-time-icon.svg"
+          alt="Date"
+        />
+      </div>
+    </div>
+  </div>
+</div>
