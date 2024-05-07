abf65ad05cdab942486495c9203b5b5173faea20
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-07 12:13:36 +0330

    add the component help

---
+++ src/components/helps/help.tsx
@@ -0,0 +1,36 @@
+<div className="m-10 size-fit">
+  <div className="bg-green-500 rounded-t-lg ">
+    <p className="text-white font-bold mr-2">آخرین کمک ها</p>
+  </div>
+  <div className="flex gap-7">
+    <div className="bg-white text-green-500 font-bold">
+      <div className="mt-2">
+        <p>کاربر علی مرادی</p>
+        <p>کاربر علی مرادی</p>
+      </div>
+    </div>
+    <div className="font-bold bg-green-100">
+      <div className="mt-2">
+        <p>مبلغ25,000تومان</p>
+        <p>مبلغ25,000تومان</p>
+      </div>
+    </div>
+
+    <div className="bg-white text-green-500 font-bold">
+      <div className="mt-2">
+        <p>کاربر علی مرادی</p>
+        <p>کاربر علی مرادی</p>
+      </div>
+    </div>
+    <div className="bg-green-100">
+      <div className="font-bold mt-2">
+        <p>مبلغ25,000تومان</p>
+        <p>مبلغ25,000تومان</p>
+      </div>
+
+      <button className="text-green-500 border-green-500 bg-white m-2 rounded-full w-24 mt-3">
+        <a href="#">بیشتر</a>
+      </button>
+    </div>
+  </div>
+</div>
