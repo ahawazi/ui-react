87d2202179304d9045a1ebf87a4d547412037de8
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-07 16:30:34 +0330

    fix plan classes

--- src/components/Plans/plan.tsx
+++ src/components/Plans/plan.tsx
@@ -1,34 +1,34 @@
-<div className="border-gray-400 border-2 rounded-xl h-fit m-11 size-fit">
-  <img
-    src="https://via.placeholder.com/300"
-    alt="Plan"
-    className="rounded-t-lg"
-  />
-  <div className="p-2">
-    <div>
-      <p className="font-semibold">
-        بیست و پنجمین مرحله ثبت نام طرح فرزندان
-        <br /> غدیر
-      </p>
-      <p className="text-gray-600 ">
-        در کنار شما قدم های بزرگی برای شادی دل
-        <br /> مردم عزیز و شعیان و محبان اهل بیت <br />
-        (علیهم سلام) برداشته ایم.
-      </p>
-    </div>
-
-    <div className="w-full h-1 bg-gray-300 justify-center"></div>
-
-    <div className="grid grid-cols-2">
-      <div></div>
-      <div className="flex gap-2 justify-end m-2">
-        <div>1402\8\17</div>
+export default function Plan() {
+  return (
+    <>
+      <div className="border-gray-400 border-2 rounded-xl">
         <img
-          className="size-6"
-          src="https://uxwing.com/wp-content/themes/uxwing/download/time-and-date/date-and-time-icon.svg"
-          alt="Date"
+          src="https://via.placeholder.com/300"
+          alt="Plan"
+          className="rounded-t-lg w-full h-32 object-cover"
         />
+        <div className="p-2 space-y-2">
+          <div className="space-y-2">
+            <p className="font-semibold">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nesciunt eveniet fuga soluta dicta distinctio, dolore aliquid voluptas quo provident expedita aut minus eum ea enim inventore itaque incidunt nemo.
+            </p>
+            <p className="text-gray-600 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nesciunt eveniet fuga soluta dicta distinctio, dolore aliquid voluptas quo provident expedita aut minus eum ea enim inventore itaque incidunt nemo.
+            </p>
+          </div>
+          <hr />  
+          <div className="flex justify-end p-2">
+            <div className="flex space-x-2 space-x-reverse">
+              <div>1402\8\17</div>
+              <img
+                className="size-6"
+                src="https://uxwing.com/wp-content/themes/uxwing/download/time-and-date/date-and-time-icon.svg"
+                alt="Date"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
+    </>
+  );
+}
