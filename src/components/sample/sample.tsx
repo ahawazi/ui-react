0022724f0b8bb38b131c87170c81c024ffd66beb
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-07 17:25:00 +0330

    fix sample class

--- src/components/sample/sample.tsx
+++ src/components/sample/sample.tsx
@@ -1,22 +1,30 @@
-<div className="rounded-lg border-4 border-purple-700 size-fit m-11">
-  <div className="bg-purple-700 h-10">
-    <p className="text-white font-bold pr-2 pt-2">خیر نمونه هفته</p>
-  </div>
-  <div className="bg-purple-100 p-2">
-    <div className="">
-      <p className="text-purple-400 font-semibold">آقای رضا اسد الهی</p>
-    </div>
+export default function Sample() {
+  return (
+    <>
+      <div className="rounded-lg border-4 border-purple-700 w-5/12 h-full m-11">
+        <div className="bg-purple-700 h-10">
+          <p className="text-white font-bold pr-2 pt-2">خیر نمونه هفته</p>
+        </div>
+        <div className="bg-purple-100 p-2 space-y-2">
+          <div className="">
+            <p className="text-purple-400 font-semibold">آقای رضا اسد الهی</p>
+          </div>
 
-    <div className="m-3">
-      <p>
-        قربانی کردن از سنت های زیبای اسلامی است که نتایج فردی و اجتماعی دارد.
-        آثار بسار زیادی که زیامحروم نماندن از آن حتی سفارش شده که قرض.
-      </p>
-    </div>
-    <div className="justify-end">
-      <button className="border border-purple-700 text-purple-700 px-6 py-1 rounded-full">
-        <a href="#">بیشتر</a>
-      </button>
-    </div>
-  </div>
-</div>
+          <div className="p-3">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam
+              nostrum quis quod praesentium in, culpa facilis fugit voluptatum
+              vel id sequi sapiente natus quos dicta, vero eaque explicabo
+              suscipit aliquam?
+            </p>
+          </div>
+          <div className="flex space-x-2 space-x-reverse justify-end items-center">
+            <a className="border border-purple-700 text-purple-700 px-5 py-0.5 rounded-full">
+              بیشتر
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
