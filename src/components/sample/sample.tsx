db5bbdbbbb15dc85d3869c109a21cdc63e39d264
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-07 12:55:56 +0330

    add the component sample

---
+++ src/components/sample/sample.tsx
@@ -0,0 +1,22 @@
+<div className="rounded-lg border-4 border-purple-700 size-fit m-11">
+  <div className="bg-purple-700 h-10">
+    <p className="text-white font-bold pr-2 pt-2">خیر نمونه هفته</p>
+  </div>
+  <div className="bg-purple-100 p-2">
+    <div className="">
+      <p className="text-purple-400 font-semibold">آقای رضا اسد الهی</p>
+    </div>
+
+    <div className="m-3">
+      <p>
+        قربانی کردن از سنت های زیبای اسلامی است که نتایج فردی و اجتماعی دارد.
+        آثار بسار زیادی که زیامحروم نماندن از آن حتی سفارش شده که قرض.
+      </p>
+    </div>
+    <div className="justify-end">
+      <button className="border border-purple-700 text-purple-700 px-6 py-1 rounded-full">
+        <a href="#">بیشتر</a>
+      </button>
+    </div>
+  </div>
+</div>
