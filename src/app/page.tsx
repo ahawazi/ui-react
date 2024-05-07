b18b9942f9ad8058086e20803ce4d252fecf4d55
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-07 12:58:20 +0330

    refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,21 @@ import BoxHeader from "@/components/elements/card/boxHeader";
 import ListItem from "@/components/elements/list/listItem";
 
 export default function Home() {
-    return (
-        <main className="min-h-screen justify-center">
-            <div className="flex">
-                <Box>
-                    <BoxHeader>
-                        <h1 className="font-bold text-indigo-600 text-xl px-4">موارد شما</h1>
-                    </BoxHeader>
-                    <ul className="divide-y-2 divide-indigo-400/20">
-                        <ListItem>
-                            عشق
-                        </ListItem>
-                        <ListItem>
-                            عشق
-                        </ListItem>
-                    </ul>
-                </Box>
-            </div>
-        </main>
-    );
+  return (
+    <main className="min-h-screen justify-center">
+      <div className="flex">
+        <Box>
+          <BoxHeader>
+            <h1 className="font-bold text-indigo-600 text-xl px-4">
+              موارد شما
+            </h1>
+          </BoxHeader>
+          <ul className="divide-y-2 divide-indigo-400/20">
+            <ListItem>عشق</ListItem>
+            <ListItem>عشق</ListItem>
+          </ul>
+        </Box>
+      </div>
+    </main>
+  );
 }
