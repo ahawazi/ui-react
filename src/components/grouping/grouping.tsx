e879610d88fe785db1f5c5d868d5f11a7938ac0b
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-08 12:57:29 +0330

    add the component Groping

---
+++ src/components/grouping/grouping.tsx
@@ -0,0 +1,19 @@
+export default function Grouping() {
+  return (
+    <>
+      <div className="space-y-3 size-fit bg-purple-900">
+        <div>
+          <p className="rounded-full text-white bg-purple-400 p-3">
+            انواع دسته بندی
+          </p>
+        </div>
+
+        <div>
+          <p className="rounded-full border-2 border-white text-white bg-purple-700 p-3">
+            انواع دسته بندی
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
