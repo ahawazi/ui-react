67d2aa1fa458d3132cfe79148a9d734314704904
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-08 12:47:58 +0330

    add the component introduction

--- src/components/Introduction/Introduction.tsx
+++ src/components/Introduction/Introduction.tsx
@@ -1,16 +1,40 @@
+import Download from "../svgs/download";
+import Play from "../svgs/play";
+import Taimer from "../svgs/taimer";
+
 export default function Introduction() {
   return (
     <>
-      <div className="flex items-center space-x-2 space-x-reverse">
-        <img
-          src="https://via.placeholder.com/40"
-          alt="Haji"
-          className="object-cover"
-        />
+      <div className="bg-purple-500 w-fit rounded-lg p-10">
+        <div className="text-left">
+          <Play />
+          <p className="text-white">
+            کلمات کلیدی: #توجه فرمایید | <a href="#">کانال ایتا موسسه</a>
+          </p>
+        </div>
 
-        <span className="text-purple-700 bg-gray-200 rounded-l-lg h-10 p-2">
-          آیت الله اعرافی
-        </span>
+        <div>
+          <div>
+            <p className="text-white font-bold">
+              بیست و پنجمین مرحله ثبت نام طرح فرزندان غدیر
+            </p>
+          </div>
+
+          <div className="text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In cum
+            tempora corporis. Officia laboriosam quisquam soluta laudantium
+            ipsam, quidem obcaecati, quibusdam distinctio voluptate atque
+            deserunt odit alias cupiditate sit. Iste.
+          </div>
+
+          <div className="flex space-x-2 space-x-reverse">
+            <p className="text-white font-bold">دانلود</p>
+            <Download />
+
+            <p className="text-white">| 20دقیقه</p>
+            <Taimer />
+          </div>
+        </div>
       </div>
     </>
   );
