832f904d0698ea99293715a8702d432ef8ee0265
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-08 11:29:35 +0330

    add the component Introduction

---
+++ src/components/Introduction/Introduction.tsx
@@ -0,0 +1,17 @@
+export default function Introduction() {
+  return (
+    <>
+      <div className="flex items-center space-x-2 space-x-reverse p-11">
+        <img
+          src="https://via.placeholder.com/40"
+          alt="Haji"
+          className="object-cover"
+        />
+
+        <span className="text-purple-700 bg-gray-200 rounded-l-lg h-10 p-2">
+          آیت الله اعرافی
+        </span>
+      </div>
+    </>
+  );
+}
