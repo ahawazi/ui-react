1fa9e1906efe948d6b1f2ed98917645c11e69360
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-08 11:59:28 +0330

    add the component plans-video

---
+++ src/components/Plans/plans-video.tsx
@@ -0,0 +1,14 @@
+import Play from "../svgs/play";
+
+export default function PlansVideo() {
+  return (
+    <>
+      <div className="rounded-md w-fit p-3 bg-slate-500">
+        <Play className="" />
+        <span className="text-white font-bold">
+          بیست و پنجمین مرحله ثبت نام طرح فرزندان غدیر
+        </span>
+      </div>
+    </>
+  );
+}
