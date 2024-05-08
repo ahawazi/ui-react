b06d7fc4e3c17318e6a4b8ab509b199caa6e9e7e
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-08 11:42:25 +0330

    add the pilgrim class

---
+++ src/components/pilgrim/pilgrim.tsx
@@ -0,0 +1,9 @@
+export default function Pilgrim() {
+  return (
+    <>
+      <div className="flex rounded-lg bg-blue-500 size-20">
+        <p className="text-white text-center">زائرین کربلا</p>
+      </div>
+    </>
+  );
+}
