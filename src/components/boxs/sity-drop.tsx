d56b2a2d91f485b13a92902e9bae15f67b9db866
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-12 23:06:36 +0330

    add the sity drop box

---
+++ src/components/boxs/sity-drop.tsx
@@ -0,0 +1,45 @@
+export default function SityDrop() {
+  return (
+    <>
+      <button
+        id="dropdownDefaultButton"
+        data-dropdown-toggle="dropdown"
+        className="text-gray-900 bg-gray-50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+        type="button"
+      >
+        شهر مورد نظر{" "}
+        <svg
+          className="w-2.5 h-2.5 ms-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+
+      <div
+        id="dropdown"
+        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
+      >
+        <ul
+          className="py-2 text-sm text-gray-700 "
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li>
+            <a href="#" className="block px-4 py-2">
+              Dashboard
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
