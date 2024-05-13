13da8eb01074df98f1409add84a9762417bd1c9b
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-13 14:51:36 +0330

    add the paln code component

---
+++ src/components/Plans/plan-code.tsx
@@ -0,0 +1,27 @@
+export default function PlanCode() {
+  return (
+    <>
+      <div>
+        <div>
+          <img
+            src="https://via.placeholder.com/300"
+            alt="Plan"
+            className="rounded-t-lg w-full h-64 object-cover"
+          />
+        </div>
+
+        <div>
+          <p>ازطریق</p>
+          <p className="font-bold text-green-400">ussdکد</p>
+        </div>
+
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum suscipit,
+          ullam quis quod ea blanditiis quia. Tempora aperiam sapiente rem
+          quisquam omnis quia natus eligendi perferendis repellendus aspernatur?
+          Dignissimos, iure.
+        </div>
+      </div>
+    </>
+  );
+}
