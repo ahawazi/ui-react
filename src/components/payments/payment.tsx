eaca2a901a9de1f9b27febcc4386b6801e966686
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-13 14:51:21 +0330

    add the payment component

---
+++ src/components/payments/payment.tsx
@@ -0,0 +1,24 @@
+import Date from "../svgs/date";
+
+export default function Payment() {
+  return (
+    <>
+      <div className="flex p-4 items-center space-x-5 space-x-reverse border border-gray-500 rounded-xl">
+        <p className="font-semibold">اقای رضا صادقی از تهران به مبلغ:</p>
+
+        <p className="font-bold text-purple-600">2.578.000ریال</p>
+
+        <p className="font-semibold">بابت:</p>
+
+        <p className="font-bold text-purple-600">نذر فرزندان غدیر</p>
+
+        <p className="font-semibold">در تاریخ:</p>
+
+        <div className="flex items-center space-x-2 space-x-reverse">
+          <p className="font-bold text-purple-600">1403/6/25</p>
+          <Date />
+        </div>
+      </div>
+    </>
+  );
+}
