1847e45ee2aebfd31fefaa20adee1180cce06d06
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-13 14:51:07 +0330

    add the manager component

---
+++ src/components/managers/manager.tsx
@@ -0,0 +1,25 @@
+export default function Manager() {
+  return (
+    <>
+      <div className="flex items-center space-x-2 space-x-reverse m-3">
+        <img
+          src="https://via.placeholder.com/100"
+          alt="Plan"
+          className="rounded-full"
+        />
+        <div>
+          <p className="text-purple-500 font-semibold">محمددستاوردی</p>
+          <p className="font-medium">مدیر عامل</p>
+        </div>
+
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+          temporibus expedita quam perspiciatis fugiat recusandae numquam
+          tempore, minus magni mollitia inventore et? Dolorum, dicta impedit.
+          Temporibus repellendus voluptatibus earum voluptates?
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+}
