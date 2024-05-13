6c8762b449da1e1eb5f2f390be8914bf797c7a57
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-13 14:50:48 +0330

    add the AmountDonated component

---
+++ src/components/amount/amount-donated.tsx
@@ -0,0 +1,14 @@
+export default function AmountDonated() {
+  return (
+    <>
+      <div className="border border-gray-400 rounded-2xl px-4 py-2 ">
+        <p className="text-white font-semibold bg-purple-500 border border-gray-400 px-4 py-2 rounded-full">
+          بیشترین مبلغ اهدایی
+        </p>
+        <p className="font-bold flex text-purple-800 gap-2">
+          2,578,000 <p className="font-thin text-purple-500">ریال</p>
+        </p>
+      </div>
+    </>
+  );
+}
