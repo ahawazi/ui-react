895f95eeb461aaa650e274777afd3988d0afd105
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-18 22:11:59 +0330

    feat (components) : add the Participation to camponent's

---
+++ src/components/participations/participation.tsx
@@ -0,0 +1,21 @@
+export default function Participation() {
+  return (
+    <>
+      <div className="bg-green-400 rounded-3xl text-left h-64">
+        <div className="pt-10 pl-10">
+          <p className="font-bold text-white text-xl">
+            مشارکت در طرح فرزندان غدیر
+          </p>
+          <p className="font-light text-white text-sm line-clamp-2 pt-4">
+            مشارکت در طرح فرزندان غدیر به جهت حل مشکل نابروری
+          </p>
+          <div className="pt-6">
+            <button className="border border-white bg-green-400 text-white py-0.5 px-6 rounded-full">
+              بیشتر
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
