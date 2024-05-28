1e052268faff44be730ba1f424f5c365264d41f1
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-05-28 14:35:31 +0330

    fix (AmountDonated) : fix the some css

--- src/components/amount/amount-donated.tsx
+++ src/components/amount/amount-donated.tsx
@@ -1,13 +1,15 @@
 export default function AmountDonated() {
   return (
     <>
-      <div className="border border-gray-400 rounded-2xl px-4 py-2 ">
-        <p className="text-white font-semibold bg-purple-500 border border-gray-400 px-4 py-2 rounded-full">
+      <div className="border border-gray-400 rounded-2xl px-4 py-2 relative w-52">
+        <p className="text-white font-semibold bg-purple-500 border border-gray-400 px-2 py-0.5 absolute -top-5 right-7 rounded-full">
           بیشترین مبلغ اهدایی
         </p>
-        <p className="font-bold flex text-purple-800 gap-2">
-          2,578,000 <p className="font-thin text-purple-500">ریال</p>
-        </p>
+
+        <article className="flex justify-center space-x-2 space-x-reverse">
+          <h3 className="text-purple-800 font-bold">2,578,000</h3>
+          <h5 className="font-normal text-purple-500">ریال</h5>
+        </article>
       </div>
     </>
   );
