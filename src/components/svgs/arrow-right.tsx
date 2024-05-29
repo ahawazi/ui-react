80fac077700d511c35fc7f4d526192a355cbe865
Mahdi Khakdaman <Khakdamanmohammadmahdi@gmail.com>
2024-05-29 10:03:21 +0330

    fix pagination styles

---
+++ src/components/svgs/arrow-right.tsx
@@ -0,0 +1,15 @@
+import classNames from "classnames";
+
+export default function ArrowRight(
+    {className = "w-6 h-6"}: Readonly<{ className?: string }> = {}
+) {
+    return <>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+             className={
+                 className
+             }>
+            <path
+                d="M6.3 2.84A1.5 1.5 0 0 0 4 4.11v11.78a1.5 1.5 0 0 0 2.3 1.27l9.344-5.891a1.5 1.5 0 0 0 0-2.538L6.3 2.841Z"/>
+        </svg>
+    </>;
+}
